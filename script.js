cc3178ae2c94979c270b7547528b0dc7fb27af0e
Alex <sasha.kovalenko0302@gmail.com>
2023-09-05 15:32:27 +0200

    fix

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 function getRandomHexColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-//  console.log(document.body);
+let text = 'm'
 
-// function test() {
-//   document.body.style.backgroundColor = getRandomHexColor();
-// }
-//  test();
-// setInterval(test, 340);
-
-// console.log(window.getComputedStyle(document.body).backgroundColor);
-const body = document.body;
-const bodystyles = window.getComputedStyle(body);
-console.log(bodystyles);
-const object = {
-  alex: {
-    barby: 1,
-  },
-  margo: {
-    baby: 10,
-  },
-};
+setInterval(() => {
+    document.body.innerText += 'm'
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log(window.getComputedStyle(document.body).backgroundColor)
+}, 2000)
